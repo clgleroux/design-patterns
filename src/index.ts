d1722768a4b21d3ce7d743707c9e08981576f318
Clément Gauthier <clement@marketparts.com>
2022-11-08 18:43:30 +0100

    :recycle: preparer le projet

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,14 @@
 import { EventManager } from './EventManager';
 import { Observer } from './interfaces/eventManager';
 import { SpaceShip } from './SpaceShip';
-import { VeryLegacyCodeFactory, VeryLegacyCodeDecoratorA, VeryLegacyCodeDecoratorB } from "./Decorator";
 
 const eventManager = EventManager.getInstance();
 
 const observerComptable: Observer = {
   update(data) {
-    console.log("Je suis observer Comptable");
+    console.log('Je suis observer Comptable');
     if (data.resultat < 4) {
-      eventManager.emit('reduction salaire', {salaire: 2});
+      eventManager.emit('reduction salaire', { salaire: 2 });
     }
-  }
-}
-
-const observerDev: Observer = {
-  update(data) {
-    console.log("Je suis observer Dev", data);
-    if (data.salaire < 3) {
-      eventManager.emit("demission", {});
-    }
-  }
-}
-
-const observerPatron: Observer = {
-  update(data) {
-    console.log(`Je suis observer Patron`);
-  }
-}
-
-// eventManager.on("mauvais resultat", observerComptable);
-// eventManager.on("reduction salaire", observerComptable);
-// eventManager.on("reduction salaire", observerDev);
-// eventManager.on("demission", observerPatron);
-
-// eventManager.emit("mauvais resultat", {resultat: 3});
-const test = new VeryLegacyCodeDecoratorA()
-console.log(test.veryComplex());
-
-const test2 = new VeryLegacyCodeDecoratorB()
-console.log(test2.veryComplex());
-
-const f = new VeryLegacyCodeFactory();
-
-console.log(f.testFactory("VeryLegacyCodeDecoratorAB").veryComplex());
-
-eventManager.on("mauvais resultat", f.testFactory("VeryLegacyEventManager").update({test: 4}));
-
-// eventManager.broadcast({data: "hello"});
+  },
+};
--- src/interfaces/decorator.ts
+++
@@ -1,3 +0,0 @@
-export interface VeryLegacy {
-  veryComplex(): string;
-}
