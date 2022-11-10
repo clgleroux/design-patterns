da09f145d96dd5acabb5c432a5820af3d748ec75
Clément Gauthier <clement@marketparts.com>
2022-11-10 09:27:51 +0100

    :bug: fix test message

--- src/Message.spec.ts
+++ src/Message.spec.ts
@@ -3,14 +3,20 @@ import { Message } from './Message';
 
 describe('Message', () => {
   let instance: Message;
-  const subject = 'mock';
-  const object = 'mock';
+  const subject = 'mockSubject';
+  const object = 'mockObject';
 
   beforeEach(() => {
     instance = new Message(subject, object);
   });
 
-  it('should message', async () => {
+  it('should class message', async () => {
     expect(instance).toBeInstanceOf(Message);
+    expect(instance.subject).toBe(subject);
+    expect(instance.object).toBe(object);
+  });
+
+  it('should send message return this', async () => {
+    expect(instance.sendMessage()).toBeInstanceOf(Message);
   });
 });
